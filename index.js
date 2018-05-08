af37b4f070e1cffdae57995b4685b8af65c6cead
Roxana <roxanafetea@yahoo.com>
2018-05-08 05:33:08 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function nestedTarget() {
   const el= document.querySelector('#nested .target');
   return el;
 }
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+ 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  
+}
+}
+
+  
+function deepestChild(){
+let node=document.getElementById('grand-node');  
+let nextnode=node.children[0];
+while (nextnode) {
+node=nextnode;
+nextnode=node.children[0]; 
+}
+return node;
+}
